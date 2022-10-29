253342a0512217b587e00bebd9a293eebd963868
Tobianointing <adeyokunnuo@gmail.com>
2022-10-29 11:42:33 +0100

    fix:Fix snapshot test breaking test and update dashboard
    remove snapshot test
    added meaningfull data to the dashboard barchart for top missing tool
    and top technologies used

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,65 +1,16 @@
-import Head from "next/head"
 
 import styles from "@/styles/dashboard.module.scss"
 import Navbar from "src/sections/Navbar/index"
 import DashboardCard from "src/components/DashboardCard/index"
 import ToolBox from "src/components/Icons/ToolBox"
-import {
-  Bar,
-  BarChart,
-  CartesianGrid,
-  Legend,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts"
-import CustomTooltip from "src/components/CustomTooltip/index"
 import PrimaryBarChart from "src/components/PrimaryBarChart/index"
 import IncompleteIcon from "src/components/Icons/IncompleteIcon"
 import EditedIcon from "src/components/Icons/EditedIcon"
-
-const data = [
-  {
-    name: "youtube",
-    uv: 4000,
-
-  },
-  {
-    name: "pywikibot",
-    uv: 3000,
-  
-  },
-  {
-    name: "torgetool",
-    uv: 2000,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-  
-  },
-]
+import {topMissingTools, topTechnologiesUsed} from "src/data"
 
 export default function Dashbaord() {
   return (
     <div className={styles.container}>
-    
-
       <Navbar />
       <main>
         <h1 className={styles.header}>Dashboard</h1>
@@ -67,17 +18,20 @@ export default function Dashbaord() {
 
         <div className={styles.cardWrapper}>
           <DashboardCard icon={<ToolBox />} title={"Total Tools"} value={"1200"} />
-          <DashboardCard icon={<IncompleteIcon />} title={"Total Tools With Missing Fields"} value={"600"} />
+          <DashboardCard
+            icon={<IncompleteIcon />}
+            title={"Total Tools With Missing Fields"}
+            value={"600"}
+          />
           <DashboardCard icon={<ToolBox />} title={"Percentage of Missing Tools"} value={"50%"} />
           <DashboardCard icon={<EditedIcon />} title={"Number of Tools Edited"} value={"800"} />
         </div>
 
         <div className={styles.chartSection}>
-          <PrimaryBarChart title={"Most missing tools"} data={data} />
-          <PrimaryBarChart title={"Most used technology"} data={data} />
+          <PrimaryBarChart title={"Most missing tools"} data={topMissingTools} />
+          <PrimaryBarChart title={"Most used technology"} data={topTechnologiesUsed} />
         </div>
       </main>
     </div>
   )
 }
-
---
+++ src/data/index.js
@@ -0,0 +1,65 @@
+export const topMissingTools = [
+  {
+    data: 22,
+    name: "toolforge-quasar",
+  },
+  {
+    data: 18,
+    name: "mm_wikidata_todo",
+  },
+  {
+    data: 10,
+    name: "toolforge-top25report-site",
+  },
+  {
+    data: 24,
+    name: "toolforge-ml-article-descriptions",
+  },
+  {
+    data: 15,
+    name: "toolforge-knwikibot",
+  },
+  {
+    data: 5,
+    name: "youtube",
+  },
+  {
+    data: 13,
+    name: "pywikibot",
+  },
+]
+
+export const topTechnologiesUsed = [
+  {
+    data: 600,
+    name: "python",
+  },
+  {
+    data: 680,
+    name: "javascript",
+  },
+  {
+    data: 300,
+    name: "ruby",
+  },
+  {
+    data: 200,
+    name: "c",
+  },
+  {
+    data: 400,
+    name: "java",
+  },
+  {
+    data: 100,
+    name: "c#",
+  },
+  {
+    data: 350,
+    name: "flutter",
+  },
+  {
+    data: 210,
+    name: "rust",
+  },
+]
